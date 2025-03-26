dda54bb70ffa16ac83e3b1fd36710f101f75579a
Mike D'Orazio <mike.dorazio@pepsico.com>
2025-03-26 18:14:18 -0400

    Added game winning calculation

--- src/ActiveCoin.jsx
+++ src/ActiveCoin.jsx
@@ -2,14 +2,15 @@ import {useState, useEffect} from 'react';
 
 export default function ActiveCoin( { currentPlayer, setCurrentPlayer, dropped, setDropped }) {
     const [row, setRow] = useState();
-    const [column, setColumn] = useState(5);
+    const [column, setColumn] = useState();
 
     function handleKeyup(event) {
         switch(event.key) {
             case 'ArrowLeft':
-                console.log("ArrowLeft");
-                if (column == 0) break;
-                setColumn(prev => prev - 1);
+                console.log("ArrowLeft", column);
+                if (column > 0) {
+                    setColumn(column - 1);
+                }
                 break;
             case 'ArrowRight':
                 console.log("ArrowRight");
@@ -20,12 +21,15 @@ export default function ActiveCoin( { currentPlayer, setCurrentPlayer, dropped, 
             case 'ArrowDown' :
             case 'Enter' :
                 console.log("ArrowDown or Enter");
-                setRow(5);
+                const totalDropped = 5 - dropped.filter(drop => drop.y == (column || 0)).length;
+                console.log("totalDropped", totalDropped);
+                if (totalDropped < 0) break;
+                setRow(totalDropped);
                 // add to dropped array only after animation is complete
                 setTimeout(() => {
                     setDropped([
                         ...dropped,
-                        {x: 5, y: column, player: currentPlayer}
+                        {x: totalDropped, y: column || 0, player: currentPlayer}
                     ]);
                     setCurrentPlayer(currentPlayer === 1 ? 2 : 1);
                 }, 500);
@@ -37,18 +41,16 @@ export default function ActiveCoin( { currentPlayer, setCurrentPlayer, dropped, 
 
     useEffect(() => {
         document.addEventListener('keyup', handleKeyup, false);
-
         return () => document.removeEventListener('keyup', handleKeyup);
     })
 
     useEffect(() => {
+        console.log("useEffect when currentPlayer changes");
         setRow();
-        setColumn(0);
+        setColumn();
     }, [currentPlayer]);
 
     return (
-
-        <div className={`active p${currentPlayer} col-${column} row-${row}`} />
-
+        <div className={`active p${currentPlayer} col-${column || 'undef'} row-${row === undefined ? 'undef' : row}`} />
     )
 }
--- src/DropZone.jsx
+++ src/DropZone.jsx
@@ -1,14 +1,53 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import ActiveCoin from './ActiveCoin';
 
 export default function DropZone() {
-    const [currentPlayer, setCurrentPlayer] = useState(2);
-    const [dropped, setDropped] = useState([
-        {x: 5, y: 4, player: 1},
-        {x: 4, y: 1, player: 2},
-        {x: 3, y: 2, player: 2},
-        {x: 1, y: 3, player: 1}
-    ]);
+    const [currentPlayer, setCurrentPlayer] = useState(1);
+    const [dropped, setDropped] = useState([]);
+    const [winner, setWinner] = useState(0);
+
+    function determineWinner() {
+        if (didPlayerWin(1)) {
+            setWinner(1);
+        };
+
+        if (didPlayerWin(2)) {
+            setWinner(2);
+        }
+
+    }
+
+    function didPlayerWin(playerNumber) {
+        const pDropped = dropped.filter(d => d.player === playerNumber);
+
+        pDropped.forEach(({x, y}) => {
+            if (pDropped.find(m => x === m.x +1 && y === m.y) &&
+                pDropped.find(m => x === m.x +2 && y === m.y) &&
+                pDropped.find(m => x === m.x +3 && y === m.y)
+            ) setWinner(playerNumber);
+            if (pDropped.find(m => x === m.x && y === m.y + 1) &&
+                pDropped.find(m => x === m.x && y === m.y + 2) &&
+                pDropped.find(m => x === m.x && y === m.y + 3)
+            ) setWinner(playerNumber);
+            if (pDropped.find(m => x === m.x + 1 && y === m.y + 1) &&
+                pDropped.find(m => x === m.x + 2 && y === m.y + 2) &&
+                pDropped.find(m => x === m.x + 3 && y === m.y + 3)
+            ) setWinner(playerNumber);
+            if (pDropped.find(m => x === m.x + 1 && y === m.y - 1) &&
+                pDropped.find(m => x === m.x + 2 && y === m.y - 2) &&
+                pDropped.find(m => x === m.x + 3 && y === m.y - 3)
+            ) setWinner(playerNumber); 
+        })
+    }
+
+    useEffect(() => {
+        if (dropped.length == 42) {
+            console.log("DRAW");
+            setWinner(-1);
+        }
+
+        determineWinner();
+    }, [dropped.length]);
 
     return (
         <div className="drop-zone">
@@ -18,7 +57,10 @@ export default function DropZone() {
                 />
             )}
 
-        <ActiveCoin currentPlayer={currentPlayer} setCurrentPlayer={setCurrentPlayer} dropped={dropped} setDropped={setDropped} />
+        {winner 
+        ?  <div>Winner {winner} </div>
+        :  <ActiveCoin currentPlayer={currentPlayer} setCurrentPlayer={setCurrentPlayer} dropped={dropped} setDropped={setDropped} />
+        }
 
         </div>
     )
