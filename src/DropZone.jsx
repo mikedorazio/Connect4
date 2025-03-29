e16df7c83a2ab6f447f3bcdb16d46f899a9937c5
Mike D'Orazio <mike.dorazio@pepsico.com>
2025-03-29 11:59:08 -0400

    Animate winning coins

--- src/DropZone.jsx
+++ src/DropZone.jsx
@@ -7,6 +7,7 @@ export default function DropZone() {
     const [dropped, setDropped] = useState([]);
     const [winner, setWinner] = useState(0);
 
+    // Check for a winner
     function determineWinner() {
         if (didPlayerWin(1)) {
             setWinner(1);
@@ -17,33 +18,86 @@ export default function DropZone() {
         }
     }
 
+    // Sets the winner state variable if there was a winner. This will end the game.
+    // Check the dropped array and see if any 4 coins are consecutive and the same color
     function didPlayerWin(playerNumber) {
         const pDropped = dropped.filter(d => d.player === playerNumber);
-
         pDropped.forEach(({x, y}) => {
             // vertical match extending downward
             if (pDropped.find(m => x === m.x + 1 && y === m.y) &&
                 pDropped.find(m => x === m.x + 2 && y === m.y) &&
                 pDropped.find(m => x === m.x + 3 && y === m.y)
-            ) setWinner(playerNumber);
+            ) {
+                setWinner(playerNumber);
+                setWinningCoins({x: x, y: y}, 0);
+            }
             // horizontal match extending to right
             if (pDropped.find(m => x === m.x && y === m.y + 1) &&
                 pDropped.find(m => x === m.x && y === m.y + 2) &&
                 pDropped.find(m => x === m.x && y === m.y + 3)
-            ) setWinner(playerNumber);
+            ) {
+                setWinner(playerNumber);
+                setWinningCoins({x: x, y: y}, 1);
+            }
             // diagonal match extending to lower right
             if (pDropped.find(m => x === m.x + 1 && y === m.y + 1) &&
                 pDropped.find(m => x === m.x + 2 && y === m.y + 2) &&
                 pDropped.find(m => x === m.x + 3 && y === m.y + 3)
-            ) setWinner(playerNumber);
+            ) {
+                setWinner(playerNumber);
+                setWinningCoins({x: x, y: y}, 2);
+            }
             // diagonal match extending to lower left
             if (pDropped.find(m => x === m.x + 1 && y === m.y - 1) &&
                 pDropped.find(m => x === m.x + 2 && y === m.y - 2) &&
                 pDropped.find(m => x === m.x + 3 && y === m.y - 3)
-            ) setWinner(playerNumber); 
+            ) {
+                setWinner(playerNumber);
+                setWinningCoins({x: x, y: y}, 3);
+            }
         })
     }
 
+    // add a winner attribute to the 4 coins that won the game. If you use an animation that alters the css transform
+    // property, it will not remember the original transform value and you will have to pass that back in again.
+    // using the simple opacity property does not have this issue and works very well. 
+    function setWinningCoins(location, direction) {
+        console.log("location, direction", location, direction);
+        // vertical
+        if (direction == 0) {
+            for (let i = 0; i < 4; i++) {
+                let elementToHighlight = document.getElementById(`x${location.x-i},y${location.y}`);
+                console.log("elementToHighlight", elementToHighlight);
+                elementToHighlight.setAttribute("winner", "yes");
+            }
+        }
+        // horizontal
+        if (direction == 1) {
+            for (let i = 0; i < 4; i++) {
+                let elementToHighlight = document.getElementById(`x${location.x},y${location.y-i}`);
+                console.log("elementToHighlight", elementToHighlight);
+                elementToHighlight.setAttribute("winner", "yes");
+            }
+        }
+        // diagonal to lower right
+        if (direction == 2) {
+            for (let i = 0; i < 4; i++) {
+                let elementToHighlight = document.getElementById(`x${location.x-i},y${location.y-i}`);
+                console.log("elementToHighlight", elementToHighlight);
+                elementToHighlight.setAttribute("winner", "yes");
+            }
+        }
+        // diagonal to lower left
+        if (direction == 3) {
+            for (let i = 0; i < 4; i++) {
+                let elementToHighlight = document.getElementById(`x${location.x-i},y${location.y+i}`);
+                console.log("elementToHighlight", elementToHighlight);
+                elementToHighlight.setAttribute("winner", "yes");
+            }
+        }
+    }
+
+    // any time there is a drop, run this effect to see if someone won or if there are no more spaces
     useEffect(() => {
         if (dropped.length == 42) {
             console.log("DRAW");
@@ -57,7 +111,7 @@ export default function DropZone() {
         <div className="drop-zone">
            {dropped.map((entry, index) =>
                 <div key={index} className={`p${entry.player}`} 
-                            drop={`x-${entry.x},y-${entry.y}`}
+                            id={`x${entry.x},y${entry.y}`}
                             style={{transform: `translate(${entry.y*100 + entry.y*5}px, ${entry.x*100+entry.x * 5 + 150}px)`}}
                 />
             )}
